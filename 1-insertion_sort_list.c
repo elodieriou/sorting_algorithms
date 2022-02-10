8027b2558c54a34ec49ea3d6b69843a76a719e05
Elodie Riou <elodie.riou@hotmail.com>
2022-02-10 16:14:13 +0100

    fix code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,54 +7,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *sorted = NULL;
 	listint_t *tmp = *list;
-	listint_t *next = NULL;
+	listint_t *left = NULL;
+	listint_t *right = NULL;
 
+    if (*list == NULL || (*list)->next == NULL)
+        return;
+
+    tmp = (*list)->next;
 	while (tmp != NULL)
 	{
-		next = tmp->next;
-		tmp->prev = NULL;
-		tmp->next = NULL;
-        printf("hello")
-		sorted_insertion(&sorted, tmp);
-
-		tmp = next;
-	}
-	*list = sorted;
-}
-
-/**
- * sorted_insertion - Function that sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: The linked list
- * @tmp: The node to insert
- * Return: Nothing
- */
-void sorted_insertion(listint_t **list, listint_t *tmp)
-{
-	listint_t *tmp2;
-
-	if (*list == NULL)
-		*list = tmp;
-
-	else if ((*list)->n >= tmp->n)
-	{
-		tmp->next = *list;
-		tmp->next->prev = tmp;
-		*list = tmp;
-	}
-
-	else
-	{
-		tmp2 = *list;
-		while (tmp2->next != NULL && tmp2->next->n < tmp->n)
-			tmp2 = tmp2->next;
-
-		if (tmp2->next != NULL)
-			tmp->next->prev = tmp;
-
-		tmp2->next = tmp;
-		tmp->prev = tmp2;
+        left = tmp->prev;
+        right = tmp->next;
+        while (tmp->n < left->n)
+        {
+            if (left->prev != NULL)
+                left->prev->next = tmp;
+            left->next = tmp->next;
+            tmp->next = left;
+            left->prev = tmp;
+            tmp->prev = left->prev;
+            if (right != NULL)
+                right->prev = left;
+            print_list(*list);
+            right = tmp->next;
+            left = tmp->prev;
+        }
+        tmp = right;
 	}
 }
