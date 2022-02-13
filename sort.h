df6d06b283c85284c6fa59b3c6e5e2ccc3f195f2
Elodie Riou <elodie.riou@hotmail.com>
2022-02-13 13:17:11 +0100

    Add function that sorts an array of integers in ascending order using the Counting sort algorithm

--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,4 +25,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+
 #endif
