dba0c3ec86bc440024ea28afffa77fedeeff02e2
Elodie Riou <elodie.riou@hotmail.com>
2022-02-13 10:48:16 +0100

    fix code

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -19,7 +19,7 @@ void shell_sort(int *array, size_t size)
 	while (interval < size / 3)
 		interval *= 3 + 1;
 
-	while (interval > 0)
+	while (interval >= 1)
 	{
 		for (i = interval; i < size; i++)
 		{
@@ -32,7 +32,7 @@ void shell_sort(int *array, size_t size)
 			}
 			array[j] = value;
 		}
-		interval = (interval - 1) / 3;
+		interval /= 3;
 		print_array(array, size);
 	}
 }
